1a846cd97527ef7b2459216375fcabbd23fc4d72
Touseef Arshad <tauseefarshad23@gmail.com>
2025-04-17 23:58:38 +0500

    for local server url added

--- clients/api.ts
+++ clients/api.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from "graphql-request";
 const isClient = typeof window !== "undefined";
 
 export const graphqlClient = new GraphQLClient(
-process.env.NEXT_PUBLIC_API_URL as string ,
+process.env.NEXT_PUBLIC_API_URL as string || 'http://localhost:4000/graphql',
   {
     headers: () => {
       const token = isClient ? localStorage.getItem("__twitter_token") : "";
