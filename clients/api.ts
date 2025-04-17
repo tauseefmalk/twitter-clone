e9caa2ccfa21641187c17661308c6b4cdcb43e79
Touseef Arshad <tauseefarshad23@gmail.com>
2025-04-17 16:52:30 +0500

    changes to API URL

--- clients/api.ts
+++ clients/api.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from "graphql-request";
 const isClient = typeof window !== "undefined";
 
 export const graphqlClient = new GraphQLClient(
-  "http://localhost:4000/graphql",
+process.env.NEXT_PUBLIC_API_URL as string ,
   {
     headers: () => {
       const token = isClient ? localStorage.getItem("__twitter_token") : "";
